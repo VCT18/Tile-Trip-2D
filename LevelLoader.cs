183904573e3d2eee8160f84e7613926b482cebec
VCT18 <congtuyenproductions@gmail.com>
2026-07-11 10:47:59 +0700

    Add files via upload

---
+++ LevelLoader.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.IO;
+
+public class LevelLoader : MonoBehaviour
+{
+    public static LevelLoader Instance;
+
+    void Awake() => Instance = this;
+
+    public LevelData LoadLevel(int levelId)
+    {
+        string fileName = $"level_{levelId:D2}.json";
+        string path = Path.Combine(Application.streamingAssetsPath, "Levels", fileName);
+
+        if (!File.Exists(path))
+        {
+            Debug.LogError($"Không tìm thấy file: {path}");
+            return null;
+        }
+
+        string json = File.ReadAllText(path);
+        return JsonUtility.FromJson<LevelData>(json);
+    }
+}
